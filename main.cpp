0473080b9045ed970447dd3fad9be22de62d6ca4
krach <krachuu64@gmail.com>
2023-05-01 12:31:29 +0200

    dlakamila

--- main.cpp
+++ main.cpp
@@ -1,45 +1,40 @@
 #include <stdio.h>
+#include <vector>
 #include <mpi.h>
 #include "utils.h"
 
 using vector2d = std::vector<std::vector<double>>;
 using vector = std::vector<double>;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     int rank, num_procs;
-    int row_size = 6;
-    int rows_per_proc = 6;
+    int row_size = 12;
+    int rows_per_proc = 2;
     vector2d spins_all;
 
-    // określamy rozmiar wektora pojedyńczego wiersza
-    // określamy rozmiar wektorów w procescie
     int total_size = row_size * rows_per_proc;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-    // tworzymy tablice wejściową
     vector2d row = generateSpins(rank, num_procs, rows_per_proc, MPI_COMM_WORLD);
-    std::cout << rank << "/" << num_procs << std::endl;
-    // printVector2D(row);
+
     vector buffer;
-    vector recv_buffer;
+    vector recv_buffer(total_size); // Resize recv_buffer to accommodate the gathered data
 
     MPI_Barrier(MPI_COMM_WORLD);
 
-    if( rank == 0){
+    if (rank == 0) {
         std::cout << "|| AFTER MPI_Barrier" << std::endl;
     }
 
-    MPI_Gather( row.data(), row_size, MPI_DOUBLE, recv_buffer.data(), 
-                row_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Gather(row.data(), row_size, MPI_DOUBLE, recv_buffer.data(), row_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
-        printVector2D( recv_buffer.data(), row_size, row_size);
+        printVector2D(recv_buffer.data(), row_size, row_size);
     }
 
-    MPI_Finalize(); 
+    MPI_Finalize();
     return 0;
 }
-
