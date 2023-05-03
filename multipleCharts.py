6d6cfa9726314336672d2f363d233d07a31f7c37
krach <krachuu64@gmail.com>
2023-05-03 11:29:12 +0200

    Added multiple colormaps saving

--- multipleCharts.py
+++ multipleCharts.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_spin_map(file_path, save_path, net_size):
+def save_colormaps(file_path, save_dir, net_size):
     with open(file_path) as f:
         lines = f.readlines()
 
     # Remove newline characters and split the lines
-    lines = [line.strip() for line in lines if line.strip()]
+    lines = [line.strip() for line in lines]
 
     # Calculate the number of grids in the file
     num_grids = len(lines) // net_size
@@ -21,25 +21,16 @@ def plot_spin_map(file_path, save_path, net_size):
 
     cmap = plt.cm.RdBu
 
-    # Set up the subplots
-    fig, ax = plt.subplots(1, 1, figsize=(num_grids * 4, 4))
-
-    # Plot each grid
+    # Save each colormap as a separate image
     for i in range(num_grids):
-        ax.imshow(config[i], cmap=cmap)
-        ax.set_title(f'Grid {i+1}')
-        if i < num_grids - 1:
-            ax = ax.twinx()
-
-    # Add a colorbar to the plot
-    cbar = fig.colorbar(ax.imshow(config[-1], cmap=cmap), ax=ax, orientation='vertical')
-
-    # Save the figure
-    plt.savefig(save_path)
-    plt.show()
+        plt.imshow(config[i], cmap=cmap)
+        plt.colorbar()
+        save_path = f"{save_dir}/colormap_{i}.png"
+        plt.savefig(save_path)
+        plt.clf()  # Clear the figure
 
 file_path = "spins.txt"
-save_path = "out.png"
+save_dir = "colormaps"
 net_size = 100
 
-plot_spin_map(file_path, save_path, net_size)
+save_colormaps(file_path, save_dir, net_size)
